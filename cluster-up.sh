6fc837bb6d3bee058d1acb3612a5eaa7d51719ac
Sam Yerkes <sam@yrks.io>
2023-05-05 09:33:36 -0400

    Use Flux's HR files

--- cluster-up.sh
+++ cluster-up.sh
@@ -20,24 +20,18 @@ sleep 10
 kubectl --context kind-kind apply -f bundle
 sleep 10
 
-# Install the Helm charts
-# ... Grafana + Prometheus
-helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
-    -n metrics 
-
-# ... Sealed Secrets
-helm install sealed-secrets \
-    sealed-secrets/sealed-secrets \
-    --set-string fullnameOverride=sealed-secrets-controller \
-    -n kube-system
-
-# ... Flux
+# Install Flux
 flux install \
     --namespace=flux-system \
     --network-policy=false \
     --components=source-controller,helm-controller
 
-sleep 5
+# Install the Helm charts
+kubectl apply -f helm/ --recursive
+
+echo ""
+echo "Letting the cluster settle..."
+sleep 45
 echo ""
 echo "Traefik: http://traefik.localhost"
-echo "Grafana: http://grafana.localhost credentials: $(kubectl get secret -n metrics kube-prometheus-stack-grafana -oyaml | grep admin-user| cut -d: -f2|tr -d \  | base64 -d):$(kubectl get secret -n metrics kube-prometheus-stack-grafana -oyaml | grep admin-password| cut -d: -f2|tr -d \  | base64 -d)"
+echo "Grafana: http://grafana.localhost credentials: $(kubectl get secret -n metrics kube-prometheus-stack-grafana -oyaml | grep admin-user | cut -d: -f2 | tr -d \  | base64 -d):$(kubectl get secret -n metrics kube-prometheus-stack-grafana -oyaml | grep admin-password | cut -d: -f2 | tr -d \ | base64 -d)"
